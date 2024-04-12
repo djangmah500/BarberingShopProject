2284b2e7ef8e64c33510052a8e63f323ed25551d
Djangma <djangmah100@gmail.com>
2024-04-12 10:26:46 +0000

    Current Customer class Implementation

---
+++ src/CurrentCustomer.java
@@ -0,0 +1,11 @@
+public class CurrentCustomer {
+    private final String name;
+
+    public CurrentCustomer(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerName() {
+        return name;
+    }
+}
